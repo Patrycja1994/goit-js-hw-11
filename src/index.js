9b42e9cc510a8b7bf1bd45e1ab4cea1b14e9ca63
Patrycja Lamik <patrycjalamik94@gmail.com>
2022-11-04 09:14:45 +0100

    zadanie 11

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const gallery = document.querySelector('.gallery');
+
+export function resultImages (result) {
+    const images = result
+    .map (image => {
+        return `<div class="photo-card">
+  <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+      ${image.likes}
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+      ${image.views}
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+      ${image.comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+      ${image.downloads}
+    </p>
+  </div>
+</div>`
+    })
+    .join('');
+
+    gallery.insertAdjacentHTML('beforeend', images);
+ }
